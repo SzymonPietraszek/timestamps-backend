e1d3ee9971f6f513e9be13204961e60ae5fa55b0
SzymonPietraszek <SzymonPietraszek1997@gmail.com>
2026-04-13 08:50:45 +0200

    Add api prefix to url

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Go to AWS and create a dynamodb with TABLE_NAME=db TABLE_PK=pk. Run `aws configu
 ```
 python -m venv venv
 source venv/bin/activate
-pip install -r requirements-dev.txt
+python -m pip install -r requirements-dev.txt
 ACTIONS="a,b,c" TIME_VALUES="0,1,2,3" TABLE_NAME=test TABLE_PK=pk uvicorn app.main:app --reload --port 8000
 deactivate
 ```
--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from app.routers import timestamps
 from mangum import Mangum
 
 app = FastAPI()
-app.include_router(timestamps.router)
+api_router = APIRouter(prefix="/api")
+api_router.include_router(timestamps.router)
 
 
-@app.get("/")
+@api_router.get("/")
 async def root():
     return {"message": "Hello World from Root!"}
 
 
+app.include_router(api_router)
 handler = Mangum(app)
